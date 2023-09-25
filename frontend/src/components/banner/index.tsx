4589dc7e091ffe550dd347aaf4018bccfcbae0b4
masha-girya <teenderrr@gmail.com>
2023-09-25 19:08:42 +0300

    [*] graphic to img

--- frontend/src/components/banner/index.tsx
+++ frontend/src/components/banner/index.tsx
@@ -1,29 +1,47 @@
 import { useState } from "react";
+import { useDevice } from "src/hooks/useDevice";
 import { Button } from "@components/button";
 import { Modal } from "@components/modal";
 import { ContactUsModal } from "@components/modals-ui/contact-us-modal";
 import styles from "./index.module.scss";
 import ShieldImg from "./assets/Shield.png";
 import RobotHandImg from "./assets/Robot-hand.png";
+import BackgroundImg from "./assets/Background.png";
+import BackgroundBottomImg from "./assets/BackgroundBottom.png";
+import BackgroundMobImg from "./assets/BackgroundMob.png";
 
 export const Banner = () => {
   const [isOpenModal, setIsOpenModal] = useState(false);
+  const { isTablet, isMobile } = useDevice();
 
   return (
     <div className={styles.banner}>
       <Modal
         isOpen={isOpenModal}
         isOpenCallback={setIsOpenModal}
-        children={<ContactUsModal setIsOpen={setIsOpenModal}/>}
+        children={<ContactUsModal setIsOpen={setIsOpenModal} />}
       />
+      <img
+        className={styles.shadow__background}
+        src={(isTablet || isMobile) ? BackgroundMobImg.src : BackgroundImg.src}
+      />
+      <img className={styles.shadow__backgroundBottom} src={BackgroundBottomImg.src} />
       <div className={styles.elements__circle}></div>
-      <div className={styles.elements__circleShadow1}></div>
-      <div className={styles.elements__circleShadow2}></div>
-      <div className={styles.elements__circleShadow3}></div>
+      <div className={styles.elements__circleBottom}></div>
       <div className={styles.banner__text}>
         <h1>Infinite AI Driven Intelligence</h1>
-        <img className={styles.banner__shieldImg_mob} src={ShieldImg.src} alt="Shield image" loading="lazy" />
-        <img className={styles.banner__robotImg_mob} src={RobotHandImg.src} alt="Robot's hand image" loading="lazy" />
+        <img
+          className={styles.banner__shieldImg_mob}
+          src={ShieldImg.src}
+          alt="Shield image"
+          loading="lazy"
+        />
+        <img
+          className={styles.banner__robotImg_mob}
+          src={RobotHandImg.src}
+          alt="Robot's hand image"
+          loading="lazy"
+        />
         <p>
           EVO provides infinite AI driven Intelligence for National Security and
           corporate organizations to make threat and opportunity related
@@ -32,13 +50,19 @@ export const Banner = () => {
         <Button name="Get Started" onClick={() => setIsOpenModal(true)} />
       </div>
       <div className={styles.banner__imgBox}>
-        <img className={styles.banner__shieldImg} src={ShieldImg.src} alt="Shield image" loading="lazy" />
-        <img className={styles.banner__robotImg} src={RobotHandImg.src} alt="Robot's hand image" loading="lazy" />
+        <img
+          className={styles.banner__shieldImg}
+          src={ShieldImg.src}
+          alt="Shield image"
+          loading="lazy"
+        />
+        <img
+          className={styles.banner__robotImg}
+          src={RobotHandImg.src}
+          alt="Robot's hand image"
+          loading="lazy"
+        />
       </div>
-      <div className={styles.elements__circleBottom}></div>
-      <div className={styles.elements__circleShadowBottom1}></div>
-      <div className={styles.elements__circleShadowBottom2}></div>
-      <div className={styles.elements__circleShadowBottom3}></div>
     </div>
   );
 };
--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -1,18 +1,24 @@
 import { Nav } from "@components/nav";
-import styles from "./index.module.scss";
-import Logo from "src/assets/images/Logo.png";
+import { useDevice } from "src/hooks/useDevice";
 import { RightsIcon } from "@components/icons/RightsIcon";
 import { SOCIALS } from "src/constants/socials";
+import styles from "./index.module.scss";
+import Logo from "src/assets/images/Logo.png";
+import BackgroundImg from "./assets/Background.png";
+import BackgroundMobImg from "./assets/BackgroundMob.png";
 
 export const Footer = () => {
+  const { isTablet, isMobile } = useDevice();
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.elements__circleShadowBottom}></div>
-      <div className={styles.elements__circleShadowBottom2}></div>
+      <img
+        className={styles.shadow__background}
+        src={isTablet || isMobile ? BackgroundMobImg.src : BackgroundImg.src}
+      />
       <div className={styles.footer__container}>
         <a href="/">
-          <img src={Logo.src} loading="lazy" />
+          <img className={styles.footer__logo} src={Logo.src} loading="lazy" />
         </a>
         <Nav isFooter />
         <div className={styles.footer__bottom}>
