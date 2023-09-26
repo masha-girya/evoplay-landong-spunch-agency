5318e1da9ad9f578f8ffb94bf6afbad40ae38b37
masha-girya <teenderrr@gmail.com>
2023-09-26 16:24:32 +0300

    [*] modal

--- frontend/src/components/button/index.tsx
+++ frontend/src/components/button/index.tsx
@@ -3,12 +3,13 @@ import classNames from "classnames";
 import styles from "./index.module.scss";
 
 export interface IButton {
-  variant?: "primary" | "middle" | "secondary" | "textMain" | "textSecondary";
+  variant?: "primary" | "middle" | "secondary" | "textMain";
   name?: string;
   onClick: () => void;
   disabled?: boolean;
   isOnLoad?: boolean;
   isActive?: boolean;
+  fullWidthMob?: boolean;
 }
 
 export const Button: React.FC<IButton> = (props) => {
@@ -18,12 +19,9 @@ export const Button: React.FC<IButton> = (props) => {
     disabled,
     isOnLoad,
     isActive,
+    fullWidthMob,
     onClick,
   } = props;
-
-  // const handleClick = () => {
-  //   if (!disabled) onClick();
-  // };
 
   return (
     <button
@@ -33,6 +31,7 @@ export const Button: React.FC<IButton> = (props) => {
       className={classNames(styles.button, {
         [styles[`button--${variant}`]]: variant,
         [styles[`button--${variant}_active`]]: isActive,
+        [styles[`button--${variant}_fullWidthMob`]]: fullWidthMob,
         [styles.button_disabled]: disabled,
       })}
     >
